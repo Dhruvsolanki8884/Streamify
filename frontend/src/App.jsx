58445e814da13de111e4ae406e40f456818c4ffe
Dhruv Solanki <solankidhaval8884@gmail.com>
2026-03-20 17:32:00 +0530

    solved import error

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,7 +7,7 @@ import NotificationsPage from "./pages/NotificationsPage.jsx";
 import CallPage from "./pages/CallPage.jsx";
 import ChatPage from "./pages/ChatPage.jsx";
 import OnboardingPage from "./pages/OnboardingPage.jsx";
-import ProfilePage from "./pages/ProfilePage.jsx";
+import ProfilePage from "./pages/pageLoader.jsx";
 
 import { Toaster } from "react-hot-toast";
 
